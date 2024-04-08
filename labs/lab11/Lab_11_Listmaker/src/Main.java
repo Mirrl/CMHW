ef8cdf7889ae98601462ea5f7a12cef03aefcd50
Mirrl <cay-fillers-0c@icloud.com>
2024-04-07 22:11:22 -0400

    pt2

--- labs/lab11/Lab_11_Listmaker/src/Main.java
+++ labs/lab11/Lab_11_Listmaker/src/Main.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+  private static ArrayList<String> list = new ArrayList<String>();
+
+  private static char promptChoiceMenu(Scanner pipe) {
+    char choice = ' ';
+
+    while (choice == ' ') {
+      System.out.println("Select an option from the menu below:");
+      System.out.println("A - Add an item to the list");
+      System.out.println("D - Delete an item from the list");
+      System.out.println("P - Print the list");
+      System.out.println("Q - Quit the program");
+
+      if (pipe.hasNextLine()) {
+        String line = pipe.nextLine();
+        if (line.length() > 0) {
+          choice = line.toUpperCase().charAt(0);
+        }
+      }
+    }
+
+    return choice;
+  }
+
+  private static void addItem(Scanner pipe) {
+    String item = SafeInput.getRegExString(pipe, "Enter an item to add to the list", ".*");
+    list.add(item);
+  }
+
+  private static void deleteItem(Scanner pipe) {
+    printList();
+    int index = SafeInput.getRangedInt(pipe, "Enter the index of the item you want to delete", 0, list.size() - 1);
+    list.remove(index);
+    if (pipe.hasNextLine())
+      pipe.nextLine();
+  }
+
+  private static void printList() {
+    if (list.isEmpty()) {
+
+    }
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println("[" + i + "]" + " - " + list.get(i));
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner pipe = new Scanner(System.in);
+
+    char choice = ' ';
+    while (choice != 'Q') {
+       choice = promptChoiceMenu(pipe);
+
+       switch (choice) {
+        case 'A':
+          addItem(pipe);
+          break;
+         case 'D':
+          deleteItem(pipe);
+          break;
+        case 'P':
+          printList();
+          break;
+        case 'Q':
+          if (!SafeInput.getYNConfirm(pipe, "Are you sure you want to quit? (Y/N)")) {
+            choice = ' ';
+          }
+          break;
+         default:
+           System.out.println("Invalid option (" + choice + ")");
+           break;
+       }
+    }
+  }
 }
